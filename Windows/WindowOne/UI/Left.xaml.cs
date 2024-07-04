216dc4cb586f00730eb30833ba953d52ade05fdd
d.aikema <djurreaikema@gmail.com>
2024-07-04 15:22:46 +0200

    -

---
+++ Windows/WindowOne/UI/Left.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Windows.WindowOne.UI;
+
+public partial class Left : UserControl
+{
+    public Left()
+    {
+        InitializeComponent();
+    }
+}
