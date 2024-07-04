8d799e2510f805c11d76b38772eb67dd3386370f
d.aikema <djurreaikema@gmail.com>
2024-07-04 15:14:47 +0200

    -

--- Windows/WindowOne/WindowOne.xaml.cs
+++ Windows/WindowOne/WindowOne.xaml.cs
@@ -4,8 +4,11 @@ namespace WpfApp1.Windows.WindowOne;
 
 public partial class WindowOne : Window
 {
+    public string HeaderText { get; set; } = "Default Header";
+
     public WindowOne()
     {
         InitializeComponent();
+        DataContext = this;
     }
 }
