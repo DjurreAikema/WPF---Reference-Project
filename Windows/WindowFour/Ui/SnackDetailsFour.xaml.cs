b7446a20d0d62e59357aae94b3f26ab872d7d5a0
Djurre <djurreaikema@gmail.com>
2024-07-10 14:18:02 +0200

    WindowFour

--- Windows/WindowFour/Ui/SnackDetailsFour.xaml.cs
+++ Windows/WindowFour/Ui/SnackDetailsFour.xaml.cs
@@ -5,16 +5,39 @@ namespace WpfApp1.Windows.WindowFour.Ui;
 
 public partial class SnackDetailsFour
 {
-    public static readonly DependencyProperty SelectedSnackProperty = DependencyProperty.Register(
-        nameof(SelectedSnack), typeof(Snack), typeof(SnackDetailsFour),
-        new PropertyMetadata(null));
+    // --- Dependency Properties
+    public static readonly DependencyProperty SelectedSnackObsProperty = DependencyProperty.Register(
+        nameof(SelectedSnackObs), typeof(IObservable<Snack>), typeof(SnackDetailsFour),
+        new PropertyMetadata(null, (d, _) =>
+        {
+            if (d is not SnackDetailsFour c) return;
+            c.Disposables.Add(c.SelectedSnackObs.Subscribe(snack =>
+            {
+                c.SelectedSnack = snack;
+                c.OnPropertyChanged(nameof(SelectedSnack));
+            }));
+        }));
+
+    public IObservable<Snack> SelectedSnackObs
+    {
+        get => (IObservable<Snack>) GetValue(SelectedSnackObsProperty);
+        set => SetValue(SelectedSnackObsProperty, value);
+    }
+
+    // --- Internal Properties
+    private Snack _selectedSnack = new();
 
     public Snack SelectedSnack
     {
-        get => (Snack) GetValue(SelectedSnackProperty);
-        set => SetValue(SelectedSnackProperty, value);
+        get => _selectedSnack;
+        set
+        {
+            _selectedSnack = value;
+            OnPropertyChanged();
+        }
     }
 
+    // --- Constructor
     public SnackDetailsFour()
     {
         InitializeComponent();
