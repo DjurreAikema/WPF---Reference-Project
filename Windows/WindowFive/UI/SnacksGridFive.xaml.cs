63ad38087b4385d5158c57cc6d0f800d87b70ce8
d.aikema <djurreaikema@gmail.com>
2024-11-21 09:48:42 +0100

    Window 5

--- Windows/WindowFive/UI/SnacksGridFive.xaml.cs
+++ Windows/WindowFive/UI/SnacksGridFive.xaml.cs
@@ -1,11 +1,54 @@
-using System.Windows.Controls;
+using System.Windows;
+using WpfApp1.Classes;
 
 namespace WpfApp1.Windows.WindowFive.UI;
 
-public partial class SnacksGridFive : UserControl
+public partial class SnacksGridFive
 {
+    // --- Dependency Properties
+    public static readonly DependencyProperty SnacksObsProperty = DependencyProperty.Register(
+        nameof(SnacksObs), typeof(IObservable<IEnumerable<Snack>>), typeof(SnacksGridFive),
+        new PropertyMetadata(null, (d, _) =>
+        {
+            if (d is not SnacksGridFive c) return;
+            c.Disposables.Add(c.SnacksObs.Subscribe(snacks =>
+            {
+                c.Snacks = snacks;
+                c.OnPropertyChanged(nameof(Snacks));
+            }));
+        }));
+
+    public IObservable<IEnumerable<Snack>> SnacksObs
+    {
+        get => (IObservable<IEnumerable<Snack>>) GetValue(SnacksObsProperty);
+        set => SetValue(SnacksObsProperty, value);
+    }
+
+    // --- Events
+    public event Action<Snack>? SnackSelected;
+
+
+    // --- Internal Properties
+    private IEnumerable<Snack>? _snacks;
+
+    public IEnumerable<Snack>? Snacks
+    {
+        get => _snacks;
+        set
+        {
+            _snacks = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // --- Constructor
     public SnacksGridFive()
     {
         InitializeComponent();
+
+        SnacksDataGrid.SelectionChanged += (_, _) =>
+        {
+            if (SnacksDataGrid.SelectedItem is Snack selectedSnack) SnackSelected?.Invoke(selectedSnack);
+        };
     }
 }
