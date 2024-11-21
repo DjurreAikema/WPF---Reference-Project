46bc882d2ee4b02252029bf7bf0fbd59571d071d
d.aikema <djurreaikema@gmail.com>
2024-11-21 12:48:44 +0100

    6

--- Windows/WindowSix/WindowSix.xaml.cs
+++ Windows/WindowSix/WindowSix.xaml.cs
@@ -1,17 +1,23 @@
 using System.Reactive.Subjects;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using WpfApp1.Classes;
-using WpfApp1.Shared;
+using WpfApp1.Windows.WindowSix.Interfaces;
 
 namespace WpfApp1.Windows.WindowSix;
 
 public partial class WindowSix
 {
-    public WindowSixViewModel ViewModel { get; } = Utils.ServiceProvider.Create<WindowSixViewModel>();
+    public WindowSixViewModel ViewModel { get; }
     public Subject<bool> TriggerDispose { get; set; } = new();
 
     public WindowSix()
     {
         InitializeComponent();
+
+        var app = (App)Application.Current;
+        var snackService = app.ServiceProvider.GetRequiredService<ISnackService>();
+        ViewModel = new WindowSixViewModel(snackService);
 
         // Dispose of all subscriptions when the window is closed
         Closing += (_, _) =>
