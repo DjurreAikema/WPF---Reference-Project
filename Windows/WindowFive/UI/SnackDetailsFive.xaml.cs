ec6c1fc8954493001d97f4c3a5b418190a79bcc4
d.aikema <djurreaikema@gmail.com>
2024-11-21 09:47:08 +0100

    Window 5

--- Windows/WindowFive/UI/SnackDetailsFive.xaml.cs
+++ Windows/WindowFive/UI/SnackDetailsFive.xaml.cs
@@ -1,9 +1,43 @@
-using System.Windows.Controls;
+using System.Windows;
+using WpfApp1.Classes;
 
 namespace WpfApp1.Windows.WindowFive.UI;
 
-public partial class SnackDetailsFive : UserControl
+public partial class SnackDetailsFive
 {
+    // --- Dependency Properties
+    public static readonly DependencyProperty SelectedSnackObsProperty = DependencyProperty.Register(
+        nameof(SelectedSnackObs), typeof(IObservable<Snack>), typeof(SnackDetailsFive),
+        new PropertyMetadata(null, (d, _) =>
+        {
+            if (d is not SnackDetailsFive c) return;
+            c.Disposables.Add(c.SelectedSnackObs.Subscribe(snack =>
+            {
+                c.SelectedSnack = snack;
+                c.OnPropertyChanged(nameof(SelectedSnack));
+            }));
+        }));
+
+    public IObservable<Snack> SelectedSnackObs
+    {
+        get => (IObservable<Snack>) GetValue(SelectedSnackObsProperty);
+        set => SetValue(SelectedSnackObsProperty, value);
+    }
+
+    // --- Internal Properties
+    private Snack _selectedSnack = new();
+
+    public Snack SelectedSnack
+    {
+        get => _selectedSnack;
+        set
+        {
+            _selectedSnack = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // --- Constructor
     public SnackDetailsFive()
     {
         InitializeComponent();
