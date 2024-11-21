5150eb2a1861687736b630dba28bdb53c6dc35cf
d.aikema <djurreaikema@gmail.com>
2024-11-21 09:44:57 +0100

    window five

--- Windows/WindowFive/WindowFive.xaml.cs
+++ Windows/WindowFive/WindowFive.xaml.cs
@@ -1,11 +1,29 @@
+using System.Reactive.Subjects;
 using System.Windows;
+using WpfApp1.Classes;
 
 namespace WpfApp1.Windows.WindowFive;
 
 public partial class WindowFive : Window
 {
+    public WindowFiveViewModel ViewModel { get; } = new();
+    public Subject<bool> TriggerDispose { get; set; } = new();
+
+    // --- Constructor
     public WindowFive()
     {
         InitializeComponent();
+
+        // Dispose of all subscriptions when the window is closed
+        Closing += (_, _) =>
+        {
+            ViewModel.Dispose();
+            TriggerDispose.OnNext(true);
+        };
+    }
+
+    private void SnacksGrid_SnackSelected(Snack snack)
+    {
+        ViewModel.SelectedSnackChanged.OnNext(snack);
     }
 }
