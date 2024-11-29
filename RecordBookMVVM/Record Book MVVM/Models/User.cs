9aa5a72c4305a6d92dbf0b88d834804134b0b783
d.aikema <djurreaikema@gmail.com>
2024-11-29 16:09:08 +0100

    Models

---
+++ RecordBookMVVM/Record Book MVVM/Models/User.cs
@@ -0,0 +1,11 @@
+namespace Record_Book_MVVM.Models;
+
+public class User
+{
+    public User()
+    {
+    }
+
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
---
+++ RecordBookMVVM/Record Book MVVM/Models/UserManager.cs
@@ -0,0 +1,17 @@
+using System.Collections.ObjectModel;
+
+namespace Record_Book_MVVM.Models;
+
+public class UserManager
+{
+    private static ObservableCollection<User> _DatabaseUsers =
+    [
+        new User() {Email = "quam@aol.couk", Name = "Erich Fry"}, new User {Email = "in@yahoo.com", Name = "Virginia Salas"},
+        new User {Email = "luctus@google.com", Name = "Alvin Velasquez"}, new User {Email = "etiam.bibendum@hotmail.edu", Name = "Zia Hutchinson"},
+        new User {Email = "augue.porttitor@protonmail.org", Name = "Justin Burch"}
+    ];
+
+    public static ObservableCollection<User> GetUsers() => _DatabaseUsers;
+
+    public static void AddUser(User user) => _DatabaseUsers.Add(user);
+}
