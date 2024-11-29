1b2bfc1a21f8665cff8be26823a9e98c865d7f13
d.aikema <djurreaikema@gmail.com>
2024-11-29 16:05:32 +0100

    Step 1

---
+++ RecordBookMVVM/Record Book MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Record_Book_MVVM.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
