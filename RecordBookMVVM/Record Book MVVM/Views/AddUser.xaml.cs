55a3db94c37cde413bedd535996df6c567d35fd7
d.aikema <djurreaikema@gmail.com>
2024-11-29 16:29:56 +0100

    Add Users

--- RecordBookMVVM/Record Book MVVM/Views/AddUser.xaml.cs
+++ RecordBookMVVM/Record Book MVVM/Views/AddUser.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using Record_Book_MVVM.ViewModel;
 
 namespace Record_Book_MVVM.Views;
 
@@ -7,5 +8,7 @@ public partial class AddUser : Window
     public AddUser()
     {
         InitializeComponent();
+        var addViewModel = new AddUserViewModel();
+        DataContext = addViewModel;
     }
 }
