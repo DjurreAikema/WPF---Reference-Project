b91f980d011f837f87de3c93976a2c6b57b61dd7
d.aikema <djurreaikema@gmail.com>
2025-03-20 11:19:33 +0100

    Remove Window8

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using WpfApp1.Windows.Window6;
 using WpfApp1.Windows.Window7;
 using WpfApp1.Windows.Window7._1;
 using WpfApp1.Windows.Window7._2;
-using WpfApp1.Windows.Window8;
 
 namespace WpfApp1;
 
@@ -71,11 +70,5 @@ public partial class MainWindow : Window
     {
         var windowSevenTwo = new WindowSevenTwo();
         windowSevenTwo.Show();
-    }
-
-    private void OpenWindowEightButton_Click(object sender, RoutedEventArgs e)
-    {
-        var windowEight = new WindowEight();
-        windowEight.Show();
     }
 }
