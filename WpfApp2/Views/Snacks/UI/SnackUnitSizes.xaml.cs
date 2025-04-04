536402f3ac1b7dc2a450b90e8601b85d89e4771b
Djurre <djurreaikema@gmail.com>
2025-04-04 22:27:43 +0200

    Improve unit size handling

--- WpfApp2/Views/Snacks/UI/SnackUnitSizes.xaml.cs
+++ WpfApp2/Views/Snacks/UI/SnackUnitSizes.xaml.cs
@@ -23,16 +23,21 @@ public partial class SnackUnitSizes
     }
 
     public static readonly DependencyProperty SelectedIdObsProperty = DependencyProperty.Register(
-        nameof(SelectedIdObs), typeof(IObservable<int>), typeof(SnackUnitSizes),
+        nameof(SelectedIdObs), typeof(IObservable<int?>), typeof(SnackUnitSizes),
         new PropertyMetadata(null, (d, _) =>
         {
             if (d is not SnackUnitSizes c) return;
-            c.Disposables.Add(c.SelectedIdObs.Subscribe(id => { c.SelectedId = id; }));
+            c.Disposables.Add(c.SelectedIdObs.Subscribe(id =>
+            {
+                c.SelectedId = id;
+                c.Selected = new UnitSize();
+                c.Selected.SnackId = c.SelectedId;
+            }));
         }));
 
-    public IObservable<int> SelectedIdObs
+    public IObservable<int?> SelectedIdObs
     {
-        get => (IObservable<int>) GetValue(SelectedIdObsProperty);
+        get => (IObservable<int?>) GetValue(SelectedIdObsProperty);
         set => SetValue(SelectedIdObsProperty, value);
     }
 
