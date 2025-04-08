2a5924da74435c94fa9935f038a8def39af402e8
d.aikema <djurreaikema@gmail.com>
2025-04-08 11:11:44 +0200

    WarehousesComboBox

--- WpfApp2/Views/Warehouses/FormUI/WarehousesComboBox.xaml.cs
+++ WpfApp2/Views/Warehouses/FormUI/WarehousesComboBox.xaml.cs
@@ -3,15 +3,15 @@ using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using WpfApp2.Data.Classes;
 
-namespace WpfApp2.Views.StamData.Countries.FormUI;
+namespace WpfApp2.Views.Warehouses.FormUI;
 
-public partial class CountriesComboBox
+public partial class WarehousesComboBox
 {
     private const int PlaceholderId = -999;
 
     // --- Dependency Properties
     public static readonly DependencyProperty SelectedIdProperty = DependencyProperty.Register(
-        nameof(SelectedId), typeof(int?), typeof(CountriesComboBox),
+        nameof(SelectedId), typeof(int?), typeof(WarehousesComboBox),
         new PropertyMetadata(null, OnSelectedIdChanged));
 
     public int? SelectedId
@@ -24,47 +24,47 @@ public partial class CountriesComboBox
     public event Action<int?>? SelectedEvent;
 
     // --- Internal Properties
-    [ObservableProperty] private ObservableCollection<Country>? _countries = [];
-    [ObservableProperty] private Country? _selected;
+    [ObservableProperty] private ObservableCollection<Warehouse>? _warehouses = [];
+    [ObservableProperty] private Warehouse? _selected;
     [ObservableProperty] private bool _isLoading;
 
-    public CountriesComboBox()
+    public WarehousesComboBox()
     {
         InitializeComponent();
 
         // Subscribe to the countries observable from the singleton ViewModel
-        Disposables.Add(CountriesService.Instance.CountriesObs.Subscribe(countries =>
+        Disposables.Add(WarehousesService.Instance.WarehousesObs.Subscribe(warehouses =>
         {
-            var countriesList = new ObservableCollection<Country>(countries);
+            var warehousesList = new ObservableCollection<Warehouse>(warehouses);
 
             // Add placeholder item if the collection is empty
-            if (!countriesList.Any())
+            if (!warehousesList.Any())
             {
-                countriesList.Add(new Country
+                warehousesList.Add(new Warehouse
                 {
                     Id = PlaceholderId,
-                    Name = "No countries found"
+                    Name = "No warehouses found"
                 });
             }
 
-            Countries = countriesList;
+            Warehouses = warehousesList;
             UpdateSelectedFromId();
         }));
 
         // Subscribe to the loading state
-        Disposables.Add(CountriesService.Instance.LoadingObs.Subscribe(loading => { IsLoading = loading; }));
+        Disposables.Add(WarehousesService.Instance.LoadingObs.Subscribe(loading => { IsLoading = loading; }));
 
-        // Trigger a reload of countries just to be sure we have the latest data
-        CountriesService.Instance.Reload.OnNext(new System.Reactive.Unit());
+        // Trigger a reload of warehouses just to be sure we have the latest data
+        WarehousesService.Instance.Reload.OnNext(new System.Reactive.Unit());
     }
 
     // --- Methods
     private void UpdateSelectedFromId()
     {
-        if (SelectedId.HasValue && Countries != null && Countries.Any())
+        if (SelectedId.HasValue && Warehouses != null && Warehouses.Any())
         {
-            var country = Countries.FirstOrDefault(c => c.Id == SelectedId);
-            if (country != null) Selected = country;
+            var warehouse = Warehouses.FirstOrDefault(c => c.Id == SelectedId);
+            if (warehouse != null) Selected = warehouse;
         }
         else if (!SelectedId.HasValue)
         {
@@ -74,7 +74,7 @@ public partial class CountriesComboBox
 
     private static void OnSelectedIdChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is CountriesComboBox selector) selector.UpdateSelectedFromId();
+        if (d is WarehousesComboBox selector) selector.UpdateSelectedFromId();
     }
 
     private void OnSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
---
+++ WpfApp2/Views/Warehouses/WarehousesService.cs
@@ -0,0 +1,21 @@
+namespace WpfApp2.Views.Warehouses;
+
+/// <summary>
+/// Provides a singleton instance of the WarehousesViewModel
+/// for use throughout the application.
+/// </summary>
+public class WarehousesService
+{
+    private static readonly Lazy<WarehousesViewModel> _instance =
+        new Lazy<WarehousesViewModel>(() => new WarehousesViewModel());
+
+    /// <summary>
+    /// Gets the singleton instance of the WarehousesViewModel.
+    /// </summary>
+    public static WarehousesViewModel Instance => _instance.Value;
+}
+
+// There's one potential concern to be aware of: since we're now using a singleton for the WarehousesViewModel,
+// the state will persist between different views that use it. This means:
+// 1. If you navigate away from the Warehouses view and come back, it will still have the same selected warehouse
+// 2. If you open the Warehouses view in a standalone window, it will share state with the main window
